4170a5129351d9f50727be62d61ebf72e87a1300
William Nascimento <williandrums@gmail.com>
2018-10-24 00:39:26 -0400

    add o componente about e add rotas ao projeto

---
+++ src/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "app/home/home.component";
+import { AboutComponent } from "app/about/about.component";
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent }//quando for encontrado a url com o nome about ele vai para o about
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+
+import { ROUTES } from 'app.routes';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES) //passando meu array de rotas para o modulo de rotas
   ],
   providers: [],
   bootstrap: [AppComponent]
