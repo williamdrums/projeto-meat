cdd92248095a3a1b297cdc73aebcdddfffaf2402
William Nascimento <williandrums@gmail.com>
2018-12-18 00:04:36 -0300

    tratando erros com operador catch

---
+++ src/app/app.error-handler.ts
@@ -0,0 +1,16 @@
+import { Response } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+
+export class ErrorHandler {
+    static handlerError(error: Response | any) {
+        let errorMessage: string
+        if (error instanceof Response) {
+            errorMessage = `Erro ${error.status} ao obter a URL ${error.url} - ${error.statusText}`
+        } else {
+            errorMessage = error.toString();
+        }
+        console.log(errorMessage)
+        return Observable.throw(errorMessage);
+    }
+}
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ErrorHandler } from '../app.error-handler'
+
 
 @Injectable()
 export class RestaurantsService {
@@ -12,6 +15,8 @@ export class RestaurantsService {
   constructor(private http: Http) { }
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+    return this.http.get(`${MEAT_API}/restaurants`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handlerError);
   }
 }
