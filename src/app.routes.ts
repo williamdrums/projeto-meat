729b70af740e499a749aa480a8b4296bd1643d7c
William Nascimento <williandrums@gmail.com>
2019-01-02 23:40:28 -0300

    Add parametros as rotas para acessar os detalhes de restaurantes

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "app/home/home.component";
 import { AboutComponent } from "app/about/about.component";
 import { RestaurantsComponent } from "app/restaurants/restaurants.component";
+import { RestaurantDetailComponent } from "app/restaurant-detail/restaurant-detail.component";
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },//quando for encontrado a url com o nome about ele vai para o about
+    { path: 'restaurants/:id', component: RestaurantDetailComponent },
     { path: 'restaurants', component: RestaurantsComponent }
 ];
---
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-menu-item',
+  templateUrl: './menu-item.component.html'
+ 
+})
+export class MenuItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
