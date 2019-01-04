405bbf477f231c703b96007fdf6ee90a5fe1fe50
William Nascimento <williandrums@gmail.com>
2019-01-04 01:04:20 -0300

    add servico http no detalhe do restaurante

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { Restaurant } from '../restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant
+  constructor(private restaurantsService: RestaurantsService,private route: ActivatedRoute) { }
 
   ngOnInit() {
+    //snapshot "como fosse  uma foto que no momento do acesso vai mostrar como estao o estado dos meus parametro"
+    //paramns objeto que contem os valores do meus parametros
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+    .subscribe(restaurant => this.restaurant = restaurant)
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -16,7 +16,13 @@ export class RestaurantsService {
 
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
-    .map(response => response.json())
-    .catch(ErrorHandler.handlerError);
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError);
+  }
+
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError);
   }
 }
